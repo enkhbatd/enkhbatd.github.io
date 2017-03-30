052e43bbc1704948ba3aedd1ce77af55492d5341
eba <enkhbat.davaasuren@rakuten.com>
2017-03-30 22:35:06 +0900

    mend

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,15 @@ function Home() {
   return (
     <div>
       <p style={s.p}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p style={s.p}>
         This is an example single page app built
         with React and React&nbsp;Router using {' '}
         {s.code('browserHistory')}. Navigate with the links below and
